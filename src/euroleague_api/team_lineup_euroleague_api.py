460eddd9204364b4401061f352ad9d0dd8f05786
YigitAsik <yyigitasikk@gmail.com>
2024-05-13 23:27:45 +0300

    made changes based on PR comments

--- src/euroleague_api/team_lineup_euroleague_api.py
+++ src/euroleague_api/team_lineup_euroleague_api.py
@@ -3,22 +3,12 @@ import numpy as np
 from euroleague_api.play_by_play_data import PlayByPlay
 from euroleague_api.boxscore_data import BoxScoreData
 
-season = 2023
-gamecode = 302
-
-pd.options.mode.chained_assignment = None
-pd.set_option('display.max_columns', None)
-pd.set_option('display.width', 170)
-pd.set_option('display.max_rows', None)
-pd.set_option('display.float_format', lambda x: '%.3f' % x)
-
 def pbp_data_with_lineups(season, game_code):
     
     # Getting pbp for the match
     pbp = PlayByPlay()
     pbp_data = pbp.get_game_play_by_play_data(season=season, gamecode=game_code)
-    pbp_data['Indices'] = range(0, len(pbp_data)) # resetting indices since going to loop through them, don't want any duplicates
-    pbp_data.set_index('Indices', inplace=True) # resetting indices since going to loop through them, don't want any duplicates
+    pbp_data = pbp_data.reset_index(drop=True)
 
     # Getting starting five
     boxscoredata = BoxScoreData()
@@ -29,25 +19,25 @@ def pbp_data_with_lineups(season, game_code):
     starting_five = starting_five.pivot(index='ID', columns='Team', values='Player')
     starting_five.reset_index(drop=True, inplace=True)
 
-    # Creating player columns
-    on_court_cols = ['Team_A1', 'Team_A2', 'Team_A3', 'Team_A4', 'Team_A5', 'Team_B1', 'Team_B2', 'Team_B3', 'Team_B4', 'Team_B5']
-    pbp_data[on_court_cols] = np.nan
-
     # Filling start of the match with starters
     on_court_mask1 = ['Team_A1', 'Team_A2', 'Team_A3', 'Team_A4', 'Team_A5']
     on_court_mask2 = ['Team_B1', 'Team_B2', 'Team_B3', 'Team_B4', 'Team_B5']
+
+    # Creating player columns
+    on_court_cols = on_court_mask1 + on_court_mask2
+    pbp_data[on_court_cols] = np.nan
 
     pbp_data.loc[0, on_court_mask1] = starting_five.iloc[:, 0].tolist()
     pbp_data.loc[0, on_court_mask2] = starting_five.iloc[:, 1].tolist()
 
     # Capturing subs and replacing player columns with players who substitute in
-    players = pbp_data.loc[min(pbp_data.index.tolist()), on_court_cols]
+    players = pbp_data.loc[0, on_court_cols]
 
-    for idx in pbp_data.index.tolist(): # looping through plays of a game
-        if pbp_data.loc[idx, 'PLAYTYPE'] == 'IN':
-            sub_time = pbp_data.loc[idx, 'MARKERTIME']
-            sub_min = pbp_data.loc[idx, 'MINUTE']
-            sub_team = pbp_data.loc[idx, 'TEAM']
+    for idx,row in pbp_data.loc[pbp_data['PLAYTYPE'].isin(['IN', 'OUT']), :].iterrows(): # looping through IN & OUTs of the game
+        if row['PLAYTYPE'] == 'IN':
+            sub_time = row['MARKERTIME']
+            sub_min = row['MINUTE']
+            sub_team = row['TEAM']
             all_subs = pbp_data.loc[(pbp_data.MINUTE == sub_min) & (pbp_data['MARKERTIME'] == sub_time) & (pbp_data.PLAYTYPE.isin(['IN', 'OUT'])), :].sort_values(by='PLAYTYPE')
 
             if pd.isnull(pbp_data.loc[max(all_subs.index), 'Team_A1']): # if I haven't made any replacement yet
@@ -88,7 +78,7 @@ def pbp_data_with_lineups(season, game_code):
                         else:
                             continue
                 
-                pbp_data.loc[max(all_subs.index), on_court_cols] = players # replace them in the actualy dataframe
+                pbp_data.loc[max(all_subs.index), on_court_cols] = players # replace them in the actual dataframe
             
             else:
                 pass
@@ -96,3 +86,7 @@ def pbp_data_with_lineups(season, game_code):
     pbp_data.loc[:, on_court_cols] = pbp_data.loc[:, on_court_cols].fillna(method='ffill') # filling every play
 
     return pbp_data # returns the dataframe
+
+df = pbp_data_with_lineups(2023, 302)
+
+df.loc[:, [col for col in df.columns if col.startswith('Team')]]
