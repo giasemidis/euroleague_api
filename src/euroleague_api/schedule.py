704c107c37c399568df1ff14fbbe3bea3fef8057
Georgios Giasemidis <g.giasemidis@gmail.com>
2026-03-24 09:26:03 +0000

    schedule fixes and docstring

--- src/euroleague_api/schedule.py
+++ src/euroleague_api/schedule.py
@@ -1,15 +1,41 @@
 import pandas as pd
+import xmltodict
 from .EuroLeagueData import EuroLeagueData
 from .utils import get_requests
 
 
-class Standings(EuroLeagueData):
-    """"""
+class Schedule(EuroLeagueData):
+    """
+    A class for getting schedule data.
+
+    Args:
+        competition (str, optional): The competition code, inherited from the
+            `EuroLeagueData` class. Choose one of:
+            - 'E' for Euroleague
+            - 'U' for Eurocup
+            Defaults to "E".
+    """
 
     def get_schedule(self, season: int) -> pd.DataFrame:
-        """"""
-        url = f"{self.BASE_URL}/{self.V1}/schedules?seasonCode={self.competition}{season}"
-        r = get_requests(url)
+        """
+        Get the schedule of a season. The schedule includes the date,
+        time, and the teams playing in each game.
+
+        The schedule gets updated as the season progresses.
+
+        Args:
+
+            season (int): The start year of the season
+
+        Returns:
+
+            pd.DataFrame: A dataframe with the schedule of the games
+        """
+        url = f"{self.BASE_URL}/{self.V1}/schedules"
+        params = {
+            "seasonCode": f"{self.competition}{season}"
+        }
+        r = get_requests(url, params=params)
         schedule_dict = xmltodict.parse(r.text)
         df = pd.DataFrame(schedule_dict["schedule"]["item"])
         df["gameday"] = df["gameday"].astype(int)
